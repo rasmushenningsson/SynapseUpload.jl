e02c8c5d969282127063781557e806b5146dc772
Rasmus Henningsson <Rasmus.Henningsson@gmail.com>
2016-06-27 15:40:23 +0200

    First implementation of SynapseUpload.jl.

---
+++ synapseupload.jl
@@ -0,0 +1,63 @@
+if length(ARGS)==1 && lowercase(ARGS[1]) in ["--help", "-help", "-h"]
+	println("Usage:")
+	println("\tjulia synapseupload.jl [folder1 folder2 ...]")
+	exit(0)
+end
+
+using SynapseClient
+include("synapseuploadimpl.jl")
+using SynapseUpload
+
+# Implementation
+# 	1. Identify all folders and files recursively.
+# 		Ignore hidden files/folders.
+# 		Ignore files/folders starting with ".".
+
+# 	2. Ask for confirmation to continue if folder already exists in Synapse.
+
+# 	3. Show summary of what will be uploaded:
+# 		Name of folder to be uploaded.
+# 		Nbr of folders.
+# 		Nbr of files with different file endings.
+# 		Total size.
+# 	4. Ask for confirmation to continue.
+
+# 	5. Create the folder specified in "Your Project/Your Folder".
+
+# 	6. Add annotation that upload is in progress
+
+# 	7. Upload files.
+# 		Create subfolders as needed. (Thus, if the upload stops, we will see how far it got.)
+
+# 	8. Add annotation that upload finished.
+
+
+sources = length(ARGS)>0 ? ARGS : ["."]
+map!(abspath,sources)
+
+println(sources)
+
+
+# TODO: error checking
+syn = SynapseClient.login()
+
+
+parentFolderID = "syn6177609"
+
+
+# prepare 
+folders = Array{FolderInfo,1}(length(sources))
+map!(listfiles, folders, sources)
+
+# check that it is ok to upload each folder
+for fi in folders
+	confirmupload(syn, parentFolderID, fi) || exit(0) # error("User abort")
+end
+
+
+# upload each folder
+for fi in folders
+	uploadfolder(syn, parentFolderID, fi)
+end
+
+
---
+++ synapseuploadimpl.jl
@@ -0,0 +1,168 @@
+module SynapseUpload
+
+export FolderInfo,
+       listfiles,
+       confirmupload,
+       uploadfolder
+
+
+using SynapseClient
+import SynapseClient: AbstractEntity, Project, Folder, File
+
+
+type FolderInfo
+	path
+	name
+	files::Array{AbstractString,1}
+	folders::Array{FolderInfo,1}
+end
+FolderInfo(path,name) = FolderInfo(path,name,[],[])
+
+
+function describefolder!(res::Dict{AbstractString,Int}, fi::FolderInfo)
+	totalSize = 0
+	nbrFolders = 1
+	for file in fi.files
+		k = splitext(file)[2]
+		res[k] = get(res,k,0)+1
+		totalSize += filesize(joinpath(fi.path,file))
+	end
+	for folder in fi.folders
+		nf,ts = describefolder!(res,folder)
+		nbrFolders += nf
+		totalSize += ts
+	end
+	nbrFolders, totalSize
+end
+
+function describefolder(fi::FolderInfo)
+	res = Dict{AbstractString,Int}()
+	nbrFolders, totalSize = describefolder!(res,fi)
+	nbrFolders, totalSize, res
+end
+
+
+function listfiles(path, parentFolderID)
+	splitPath = split(path, ['/','\\'])
+	while isempty(splitPath[end])
+		pop!(splitPath)
+	end
+	isempty(splitPath) && error("\"$path\" is not a valid path")
+	name = splitPath[end]
+
+
+	folderInfo = FolderInfo(path, name)
+
+	for file in readdir(path)
+		isempty(file) && continue
+		file[1] == '.' && continue
+
+		fullFilePath = joinpath(path, file)
+		if isdir(fullFilePath)
+			# println("folder: $file")
+			push!(folderInfo.folders, listfiles(fullFilePath))
+		else
+			# println("file: $file")
+			push!(folderInfo.files, file)
+		end
+	end
+
+	folderInfo
+end
+
+
+function getchildbyname(syn, parentID::AbstractString, child::AbstractString)
+	results = chunkedquery(syn, "select id from entity where entity.parentId=='$parentID' and entity.name=='$child'")
+
+	# # if 0 results, return "",
+	# # if 1 result, return it
+	# # if 2 results, error
+
+	res = ""
+	for (i,r) in enumerate(results)
+		i>1 && error("Unexpected error, multiple children with the same name.")
+		res = r["entity.id"]
+	end
+	res::ASCIIString
+end
+getchildbyname(syn, parent::AbstractEntity, child::AbstractString) = getchildbyname(syn, parent["id"],child)
+
+
+function askforconfirmation(str::AbstractString)
+	while true
+		println(str, " (y/n)")
+		response = strip(readline())
+		lowercase(response) in ["y","yes"] && return true
+		lowercase(response) in ["n","no"] && return false
+	end
+end
+
+function nbrbytes2string(x::Integer)
+	x < 1024 && return string(x, " bytes")
+	y = Float64(x)/1024
+	y < 1024 && return @sprintf("%.1f KB", y)
+	y /= 1024
+	y < 1024 && return @sprintf("%.1f MB", y)
+	y /= 1024
+	y < 1024 && return @sprintf("%.1f GB", y)
+	y /= 1024
+	@sprintf("%.1f TB", y)
+end
+
+
+function confirmupload(syn::Synapse, parentFolderID::AbstractString, fi::FolderInfo)
+	child = getchildbyname(syn, parentFolderID, fi.name)
+	if !isempty(child)
+		askforconfirmation("Folder \"$(fi.name)\" already exists, continue?") || return false
+	end
+
+	nbrFolders, totalSize, desc = describefolder(fi)
+
+	println("--- Summary for $(fi.name) ---")
+	println("Number of folders: $nbrFolders")
+	println("Number of files:")
+	for (k,v) in desc
+		println("\t$k: $v")
+	end
+	println("Total size: ", nbrbytes2string(totalSize))
+
+	askforconfirmation("Continue?")
+end
+
+
+function _uploadfolder(syn::Synapse, parentID::AbstractString, fi::FolderInfo)
+	folder = getchildbyname(syn, parentID, fi.name)
+	if isempty(folder)
+		# create folder
+		folder = Folder(fi.name, parent=parentID)
+		folder = store(syn,folder)
+	end
+
+	annot = getannotations(syn, folder)
+	annot["uploading"] = "In progress"
+	setannotations(syn, folder, annot)
+
+	for filename in fi.files
+		# TODO: set contentType?
+		file = File( path=joinpath(fi.path,filename), name=filename, parent=folder)
+		store(syn,file)
+	end
+
+	for subfolder in fi.folders	
+		_uploadfolder(syn,folder,subfolder)
+	end
+
+	annot = getannotations(syn, folder)
+	annot["uploading"] = "Finished"
+	setannotations(syn, folder, annot)
+end
+
+_uploadfolder(syn::Synapse, parent::Folder, fi::FolderInfo) = _uploadfolder(syn, parent["id"], fi)
+
+
+function uploadfolder(syn::Synapse, parentFolderID::AbstractString, fi::FolderInfo)
+	_uploadfolder(syn,parentFolderID,fi)
+end
+
+
+end
